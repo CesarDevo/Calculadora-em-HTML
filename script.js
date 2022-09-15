6954973f3ce47c41c35f1cbfeec73bfda4b389dd
CesarDevo <108280943+CesarDevo@users.noreply.github.com>
2022-09-15 18:15:42 -0300

    programei o clique no botão 8

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let sete = document.getElementById('sete');
+let oito = document.getElementById('oito');
 let mais = document.getElementById('plus');
 let input = document.getElementById('input');
 let x = 0;
@@ -8,6 +9,11 @@ let sum = 0;
 sete.addEventListener("click", () => {
   input.value = "7";
   x = 7;
+});
+
+oito.addEventListener("click", () => {
+  input.value = "8";
+  x = 8;
 });
 
 mais.addEventListener("click", () => {
